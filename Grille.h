88e8d06c4147e8d22e994028532494fa02afd7c3
marthimontaha <marthimontaha@gmail.com>
2025-11-23 18:22:36 +0100

    Update Grille.h

--- Grille.h
+++ Grille.h
@@ -16,3 +16,21 @@ class Grille {
 
 
 #endif //PROJET_POO_GRILLE_H
+
+
+#include "Cellule.h"
+#include <vector>
+
+class Grille {
+private:
+    int largeur;
+    int longueur; 
+    std::vector<std::vector<Cellule>> cellules;
+public:
+    Grille(int largeur, int longueur);
+    int getLargeur();
+    int getLongueur(); 
+    Cellule& getCellule(int x, int y);
+    int compterVoisinVivants(int x, int y);
+
+};
