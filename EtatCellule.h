6067cfce0a9eeeefe231adeb1bf6f4c37ab91b07
Daoud <daoud.munchen@gmail.com>
2025-11-23 19:50:37 +0100

    Update EtatCellule.h

--- EtatCellule.h
+++ EtatCellule.h
@@ -1,9 +1,14 @@
-#ifndef PROJET_POO_ETATCELLULE_H
-#define PROJET_POO_ETATCELLULE_H
+#pragma once
 
-
+// Interface abstraite (State Pattern)
 class EtatCellule {
+public:
+    virtual ~EtatCellule() = default;
+
+    // Méthodes virtuelles pures
+    virtual bool estVivante() const = 0;
+    virtual char getSymbole() const = 0;
+    
+    // Pattern Prototype : permet de dupliquer l'état sans connaitre son type concret
+    virtual EtatCellule* cloner() const = 0;
 };
-
-
-#endif //PROJET_POO_ETATCELLULE_H
