6c4701d6d74d4815f6ff06bfd8446793d8417993
PaulG111 <paulgindre@icloud.com>
2025-11-24 15:58:38 +0100

    Update EtatMort.cpp

--- EtatMort.cpp
+++ EtatMort.cpp
@@ -1,2 +1,11 @@
-
 #include "EtatMort.h"
+
+bool EtatMort::estVivante() const {
+  return false;
+}
+char EtatMort::getSymbole() const {
+  return "0"
+}
+EtatCellule*EtatMort::clone() const {
+  return new EtatMort(*this);
+}
