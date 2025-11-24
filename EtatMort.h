35d1f1c75091da95262833987fd499088da704a9
PaulG111 <paulgindre@icloud.com>
2025-11-24 15:08:34 +0100

    Update EtatMort.h

--- EtatMort.h
+++ EtatMort.h
@@ -3,6 +3,7 @@
 class EtatMort : public EtatCellule {
 public :
   etatmort() = default;
-  bool estVivant() const {return false}
-  char getSymbole() const {return "0"}
+  bool estVivant() const override {return false}
+  char getSymbole() const override {return "0"}
+  EtatCellule* clone() const override { return new EtatMort(*this); }
 };
