5210bfd966333095dbd978642e6eb12381593a07
Daoud <daoud.munchen@gmail.com>
2025-11-26 21:30:03 +0100

    Update EtatVivant.cpp

--- EtatVivant.cpp
+++ EtatVivant.cpp
@@ -1,2 +1,13 @@
-
 #include "EtatVivant.h"
+
+bool EtatVivant::estVivante() const {
+  return true;
+}
+
+char EtatMort::getSymbole() const {
+  return "1";
+}
+
+EtatCellule*EtatVivant::clone() const {
+  return new EtatVivant(*this);
+}
