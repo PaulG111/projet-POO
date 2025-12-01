106107aa6de7d20f6f093618421294325e369fd4
Daoud <daoud.munchen@gmail.com>
2025-12-01 14:17:12 +0100

    Update RegleConway.cpp

--- RegleConway.cpp
+++ RegleConway.cpp
@@ -1,3 +1,17 @@
-#include "RegleConway.h"
-#include "EtatVivant.h"
-#include "EtatMort.h"
+#include "RegleConway.hpp"
+#include "EtatVivant.hpp"
+#include "EtatMort.hpp"
+
+EtatCellule* RegleConway::calculerProchainEtat(const EtatCellule& etatActuel, int nbVoisins) const {
+    
+    bool estVivant = etatActuel.estVivante();
+
+    if (estVivant) {
+      
+    } 
+    else {
+  
+    }
+    
+    return nullptr; 
+}
