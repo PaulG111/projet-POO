ae14a7ef285e55427aea442b86524f373c3d9ba8
Daoud <daoud.munchen@gmail.com>
2025-12-04 10:01:08 +0100

    Update IVue.h

--- IVue.h
+++ IVue.h
@@ -8,6 +8,8 @@ public:
     virtual ~IVue() = default;
 
     virtual void afficher(const Grille& grille, int iteration) = 0;
+    virtual bool gererEntree() = 0;
+    virtual bool estEnPause() const =0;
 };
 
 #endif //PROJET_POO_IVUE_H
