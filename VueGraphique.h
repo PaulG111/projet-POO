7ef6aa414eaf7d18e5a2a6cce2cf28ed6e79e437
Daoud <daoud.munchen@gmail.com>
2025-12-04 19:36:38 +0100

    Update VueGraphique.h

--- VueGraphique.h
+++ VueGraphique.h
@@ -1,4 +1,3 @@
-#pragma once
 #include "IVue.h"
 #include <SFML/Graphics.hpp>
 
@@ -8,6 +7,9 @@ private:
     float tailleCellule;
     bool enPause;
     int delaiMs = 500;
+    sf::Texture textureAide;
+    sf::Sprite spriteAide;  
+    bool imageChargee; 
 
 public:
     VueGraphique(int largeurGrille, int hauteurGrille);
@@ -15,4 +17,5 @@ public:
     void afficher(const Grille& grille, int iteration) override;
     bool gererEntree();
     bool estEnPause() const;
+    bool attendreToucheEntree() override;
 };
